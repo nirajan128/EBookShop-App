61d14db076dea34073d04d5c3fc70e1706c025c5
Nirajan <nirajanshrestha672@gmail.com>
2025-01-05 13:08:13 -0500

    [Section 3.0.1]: Generic Repository Interface and class - Scoped Dependency Injection

---
+++ EBookShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookShop.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T: class
+    {
+        //T-Category, Where T is a Category Class
+        //Different method related to it
+
+        //Get All the Category from db
+        IEnumerable<T> GetAll(); //COllections
+
+        //Get a single category
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        void Remove(T entity);
+        void Add(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
