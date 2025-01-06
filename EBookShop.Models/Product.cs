61bb8f7e4e445bfed21ee32adc4c72098dd258a6
Nirajan <nirajanshrestha672@gmail.com>
2025-01-06 14:15:39 -0500

    [Section 3.0.6]: Added Product Model and Instance of it on ApplicationDbContext

---
+++ EBookShop.Models/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookShop.Models
+{
+    public class Product
+    {
+        [Key] //optional, since the peoperty name is Id
+        public int Id { get; set; }
+
+        //Annotations are like a lable for columns, primary key or foreign key
+        //Usually written as  [annotaion], if the propery is Id, classNameID then the
+        //Entity framework automatically thinks its a primary key
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+    }
+}
+}
--- EBookShop.Models/obj/Debug/net8.0/EBookShop.Models.AssemblyInfo.cs
+++ EBookShop.Models/obj/Debug/net8.0/EBookShop.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EBookShop.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97839d229f54957f17b240db7c6e38d54865e51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2b288dd8a8bf82728ea33824ed338eb31c9715")]
 [assembly: System.Reflection.AssemblyProductAttribute("EBookShop.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EBookShop.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
